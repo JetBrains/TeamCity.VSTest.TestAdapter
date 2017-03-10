fd4c29bf07972a49ed1e5e3f575676cbe8a119b2
Nikolay.Pianikov <Nikolay.Pianikov@jetbrains.com>
2017-03-10 18:30:41 +0300

    Add MS .net teste

--- runtests.cmd
+++ runtests.cmd
@@ -1,5 +1,5 @@
 rem SET VSTEST_HOST_DEBUG=1
 rem SET VSTEST_RUNNER_DEBUG=1
 rem SET TEAMCITY_PROJECT_NAME=aaa
-rem dotnet test IntegrationTests\dotNetCore.XUnit.Tests\dotNetCore.XUnit.Tests.csproj /p:VSTestLogger=teamcity;VSTestTestAdapterPath=.
+dotnet test IntegrationTests\dotNetCore.XUnit.Tests\dotNetCore.XUnit.Tests.csproj /p:VSTestLogger=teamcity;VSTestTestAdapterPath=.
 dotnet test IntegrationTests\dotNetCore.MS.Tests\dotNetCore.MS.Tests.csproj /p:VSTestLogger=teamcity;VSTestTestAdapterPath=.
