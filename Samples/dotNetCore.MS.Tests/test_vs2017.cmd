03640a886b2409602182650447bf579ce858a501
Nikolay.Pianikov <Nikolay.Pianikov@jetbrains.com>
2017-03-16 16:39:32 +0300

    minor

--- Samples/dotNetCore.MS.Tests/test_vs2017.cmd
+++
@@ -1,3 +0,0 @@
-SET TEAMCITY_PROJECT_NAME=myproj
-dotnet restore
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" Debug\netcoreapp1.0\dotNetCore.MS.Tests.dll /Logger:teamcity /TestAdapterPath:.
