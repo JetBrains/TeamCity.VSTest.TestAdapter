88263d403261f2b6455903249353730f23023b85
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2018-12-11 17:20:34 +0300

    #26 Support test metadata

---
+++ TeamCity.VSTest.TestLogger/IdGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamCity.VSTest.TestLogger
+{
+    using System;
+    using System.Linq;
+
+    internal class IdGenerator : IIdGenerator
+    {
+        public string NewId() => new string(Guid.NewGuid().ToString().Where(c => c != '-').ToArray());
+    }
+}
--- TeamCity.VSTest.TestLogger/Options.cs
+++ TeamCity.VSTest.TestLogger/Options.cs
@@ -1,7 +1,24 @@
 ﻿namespace TeamCity.VSTest.TestLogger
 {
+    using System;
+
     internal class Options : IOptions
     {
+        private static readonly TeamCityVersion VersionVal = new TeamCityVersion(Environment.GetEnvironmentVariable("TEAMCITY_VERSION"));
+        private static readonly string RootFlowIdVal = Environment.GetEnvironmentVariable("TEAMCITY_PROCESS_FLOW_ID");
+        private static readonly bool AllowExperimentalVal = GetBool(Environment.GetEnvironmentVariable("TEAMCITY_LOGGER_ALLOW_EXPERIMENTAL"), true);
+        private static readonly TeamCityVersion TestMetadataSupportVersionVal = new TeamCityVersion("2018.2");
+
         public string TestRunDirectory { get; set; }
+        
+        public TeamCityVersion Version => VersionVal;
+
+        public string RootFlowId => RootFlowIdVal;
+
+        public bool AllowExperimental => AllowExperimentalVal;
+
+        public TeamCityVersion TestMetadataSupportVersion => TestMetadataSupportVersionVal;
+
+        private static bool GetBool(string value, bool defaultValue) => !string.IsNullOrEmpty(value) && bool.TryParse(value, out var result) ? result : defaultValue;
     }
 }
