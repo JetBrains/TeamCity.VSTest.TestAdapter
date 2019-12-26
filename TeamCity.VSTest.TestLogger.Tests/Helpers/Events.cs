f60d9a983fe5714a5b9f662be341ba96d9b2487c
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2019-12-26 13:02:05 +0300

    Fix build

--- TeamCity.VSTest.TestLogger.Tests/Helpers/Events.cs
+++ TeamCity.VSTest.TestLogger.Tests/Helpers/Events.cs
@@ -9,6 +9,11 @@
         public override event EventHandler<TestRunMessageEventArgs> TestRunMessage;
         public override event EventHandler<TestResultEventArgs> TestResult;
         public override event EventHandler<TestRunCompleteEventArgs> TestRunComplete;
+        public override event EventHandler<TestRunStartEventArgs> TestRunStart;
+        public override event EventHandler<DiscoveryStartEventArgs> DiscoveryStart;
+        public override event EventHandler<TestRunMessageEventArgs> DiscoveryMessage;
+        public override event EventHandler<DiscoveredTestsEventArgs> DiscoveredTests;
+        public override event EventHandler<DiscoveryCompleteEventArgs> DiscoveryComplete;
 
         public void SendTestRunMessage(TestRunMessageEventArgs ev)
         {
