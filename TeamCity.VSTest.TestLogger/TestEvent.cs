40bd27b7f0d726a9be0c5a58ee1f8d6f313ec47f
Nikolay Pianikov <nikolayp@mail.ru>
2023-12-08 18:48:19 +0300

    #46 Unable to retrieve test parameters from parameterized xunit tests - adds a resultDisplayName attribute

--- TeamCity.VSTest.TestLogger/TestEvent.cs
+++ TeamCity.VSTest.TestLogger/TestEvent.cs
@@ -1,16 +1,10 @@
-namespace TeamCity.VSTest.TestLogger
+namespace TeamCity.VSTest.TestLogger;
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+public class TestEvent(string suiteName, string displayName, TestCase testCase)
 {
-    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-
-    public class TestEvent
-    {
-        public readonly string SuiteName;
-        public readonly TestCase TestCase;
-
-        public TestEvent(string suiteName, TestCase testCase)
-        {
-            SuiteName = suiteName;
-            TestCase = testCase;
-        }
-    }
+    public readonly string SuiteName = suiteName;
+    public readonly string DisplayName = displayName;
+    public readonly TestCase TestCase = testCase;
 }
--- TeamCity.VSTest.TestLogger/TestInfoUpdater.cs
+++ TeamCity.VSTest.TestLogger/TestInfoUpdater.cs
@@ -1,59 +1,59 @@
-namespace TeamCity.VSTest.TestLogger
+namespace TeamCity.VSTest.TestLogger;
+
+using System;
+using JetBrains.TeamCity.ServiceMessages;
+using JetBrains.TeamCity.ServiceMessages.Write.Special;
+
+internal class TestInfoUpdater(IEventContext eventContext) : IServiceMessageUpdater
 {
-    using System;
-    using JetBrains.TeamCity.ServiceMessages;
-    using JetBrains.TeamCity.ServiceMessages.Write.Special;
-
-    internal class TestInfoUpdater: IServiceMessageUpdater
+    public IServiceMessage UpdateServiceMessage(IServiceMessage message)
     {
-        private readonly IEventContext _eventContext;
-
-        public TestInfoUpdater(IEventContext eventContext) => _eventContext = eventContext;
-
-        public IServiceMessage UpdateServiceMessage(IServiceMessage message)
+        if (message == null) throw new ArgumentNullException(nameof(message));
+        if (!eventContext.TryGetEvent(out var testEvent))
         {
-            if (message == null) throw new ArgumentNullException(nameof(message));
-            if (!_eventContext.TryGetEvent(out var testEvent))
-            {
-                return message;
-            }
-            
-            var newMessage = new PatchedServiceMessage(message);
-            if (!string.IsNullOrEmpty(testEvent.SuiteName))
-            {
-                newMessage.Add("suiteName", testEvent.SuiteName);
-            }
-            
-            if (!string.IsNullOrEmpty(testEvent.TestCase.Source))
-            {
-                newMessage.Add("testSource", testEvent.TestCase.Source);
-            }
-            
-            if (!string.IsNullOrEmpty(testEvent.TestCase.DisplayName))
-            {
-                newMessage.Add("displayName", testEvent.TestCase.DisplayName);
-            }
-            
-            if (!string.IsNullOrEmpty(testEvent.TestCase.CodeFilePath))
-            {
-                newMessage.Add("codeFilePath", testEvent.TestCase.CodeFilePath);
-            }
-            
-            if (!string.IsNullOrEmpty(testEvent.TestCase.FullyQualifiedName))
-            {
-                newMessage.Add("fullyQualifiedName", testEvent.TestCase.FullyQualifiedName);
-            }
-
-            newMessage.Add("id", testEvent.TestCase.Id.ToString());
-            
-            if (testEvent.TestCase.ExecutorUri != default)
-            {
-                newMessage.Add("executorUri", testEvent.TestCase.ExecutorUri.ToString());
-            }
-            
-            newMessage.Add("lineNumber", testEvent.TestCase.LineNumber.ToString());
-
-            return newMessage;
+            return message;
         }
+            
+        var newMessage = new PatchedServiceMessage(message);
+        if (!string.IsNullOrEmpty(testEvent.SuiteName))
+        {
+            newMessage.Add("suiteName", testEvent.SuiteName);
+        }
+            
+        if (!string.IsNullOrEmpty(testEvent.TestCase.Source))
+        {
+            newMessage.Add("testSource", testEvent.TestCase.Source);
+        }
+            
+        if (!string.IsNullOrEmpty(testEvent.TestCase.DisplayName))
+        {
+            newMessage.Add("displayName", testEvent.TestCase.DisplayName);
+        }
+            
+        if (!string.IsNullOrEmpty(testEvent.DisplayName))
+        {
+            newMessage.Add("resultDisplayName", testEvent.DisplayName);
+        }
+            
+        if (!string.IsNullOrEmpty(testEvent.TestCase.CodeFilePath))
+        {
+            newMessage.Add("codeFilePath", testEvent.TestCase.CodeFilePath);
+        }
+            
+        if (!string.IsNullOrEmpty(testEvent.TestCase.FullyQualifiedName))
+        {
+            newMessage.Add("fullyQualifiedName", testEvent.TestCase.FullyQualifiedName);
+        }
+
+        newMessage.Add("id", testEvent.TestCase.Id.ToString());
+            
+        if (testEvent.TestCase.ExecutorUri != default)
+        {
+            newMessage.Add("executorUri", testEvent.TestCase.ExecutorUri.ToString());
+        }
+            
+        newMessage.Add("lineNumber", testEvent.TestCase.LineNumber.ToString());
+
+        return newMessage;
     }
 }
